ae5bd34e916b25ba9108931e7a775678078fa548
Samuel <samuelh08.shm@gmail.com>
2022-04-11 16:18:31 -0500

    Adds Favs model and CRUD

--- server/api/v1/favs/controller.js
+++ server/api/v1/favs/controller.js
@@ -1,26 +1,79 @@
-exports.create = (req, res) => {
+const Model = require('./model');
+
+exports.id = async (req, res, next) => {
+  const { params = {} } = req;
+  const { id } = params;
+
+  try {
+    const doc = await Model.findById(id);
+    if (!doc) {
+      const message = 'Favs not found';
+      next({
+        message,
+        statusCode: 404,
+      });
+    } else {
+      req.doc = doc;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.create = async (req, res, next) => {
   const { body = {} } = req;
+  try {
+    const model = new Model(body);
+    const doc = await model.save();
+    res.status(201);
+    res.json({
+      data: doc,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.list = async (req, res, next) => {
+  try {
+    const docs = await Model.find({}).exec();
+    res.json({
+      data: docs,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.read = async (req, res) => {
+  const { doc = {} } = req;
   res.json({
-    data: body,
+    data: doc,
   });
 };
 
-exports.list = (req, res) => {
-  res.json([]);
+exports.update = async (req, res, next) => {
+  const { doc = {}, body = {} } = req;
+  Object.assign(doc, body);
+  try {
+    const updated = await doc.save();
+    res.json({
+      data: updated,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.read = (req, res) => {
-  const { params = {} } = req;
-  const { id } = params;
-  res.json({
-    id,
-  });
-};
-
-exports.delete = (req, res) => {
-  const { params = {} } = req;
-  const { id } = params;
-  res.json({
-    id,
-  });
+exports.delete = async (req, res, next) => {
+  const { doc = {} } = req;
+  try {
+    const deleted = await doc.remove();
+    res.json({
+      data: deleted,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
